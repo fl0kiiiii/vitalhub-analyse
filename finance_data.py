43c6b60712ae01b55c3cb9a754fef902f8077c96
fl0kiiiii <n1ung.bkk@gmail.com>
2025-04-21 18:08:06 +0000

    Update finance_data.py

--- finance_data.py
+++ finance_data.py
@@ -1,21 +1,25 @@
-import yfinance as yf
-
 def get_financial_data(ticker):
     stock = yf.Ticker(ticker)
     info = stock.info
+
+    revenue = info.get("totalRevenue")
+    net_income = info.get("netIncome")
+    gross_margins = info.get("grossMargins")
+    ebitda_margins = info.get("ebitdaMargins")
 
     data = {
         "name": info.get("longName"),
         "symbol": ticker,
         "price": info.get("currentPrice"),
         "marketCap": info.get("marketCap"),
-        "revenue": info.get("totalRevenue"),
+        "revenue": revenue,
+        "netIncome": net_income,
         "peRatio": info.get("trailingPE"),
         "psRatio": info.get("priceToSalesTrailing12Months"),
         "freeCashflow": info.get("freeCashflow"),
-        "grossMargins": info.get("grossMargins"),
-        "ebitda": info.get("ebitda"),
-        "ebitdaMargins": info.get("ebitdaMargins"),
+        "grossMargins": gross_margins,
+        "ebitdaMargins": ebitda_margins,
+        "netMargin": (net_income / revenue) if net_income and revenue else None,
         "debtToEquity": info.get("debtToEquity"),
         "beta": info.get("beta"),
         "sector": info.get("sector"),
